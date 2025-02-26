408585e92c9496b2e5d40ae3294c169c8a590db6
Sufalthakre18 <sufalthakre1@gmail.com>
2025-02-26 13:02:36 +0000

    project_js02

---
+++ projects_js02/home.js
@@ -0,0 +1,27 @@
+const form=document.querySelector('form');
+// this use case will give you empty
+// const height=parseInt(document.querySelector('#height').value)
+form.addEventListener('submit',function (e) {
+    e.preventDefault();
+    const height=parseInt(document.querySelector('#height').value);
+    const weight=parseInt(document.querySelector('#weight').value);
+    const result=document.querySelector('#results');
+    const guide=document.querySelector('#Guide');
+
+    if (height===''|| height<0||isNaN(height)) {
+        result.innerHTML=`please give valid height :- ${height}`;
+    }else if (weight===''|| weight<0||isNaN(weight)) {
+        result.innerHTML=`please give valid weight :- ${weight}`;
+    } else{
+        const bmi=(weight/((height*height)/10000)).toFixed(2)
+        result.innerHTML=`<span>${bmi}</span>`;
+        if (bmi<18.6) {
+            guide.innerHTML=`you are Under Weight`
+        }else if (bmi>=18.6 && bmi<=24.9) {
+            guide.innerHTML=`you are in Normal weight`
+        }else{
+            guide.innerHTML=`you are Overweight`
+        }
+    }
+
+})
